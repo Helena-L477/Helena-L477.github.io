cf8ac7ccdf29a804ce1c44b4ded1e9bf406db190
Helena-L477 <134717379+Helena-L477@users.noreply.github.com>
2025-01-24 17:34:40 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+
+function toggleDialog() {
+    const dialogBackground = document.getElementById("dialog-background");
+    const dialog = document.getElementById("dialog");
+
+    dialogBackground.classList.toggle("show");
+    dialog.classList.toggle("show");
+
+}
